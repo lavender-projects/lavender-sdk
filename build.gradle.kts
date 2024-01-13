979c86898c195d05ca41151aa14acb4f25051a2d
Kosaka Bun <kosaka-bun@qq.com>
2024-01-14 00:25:53 +0800

    Update Versions.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,18 @@
 import de.honoka.gradle.buildsrc.MavenPublish.checkVersionOfProjects
 import de.honoka.gradle.buildsrc.MavenPublish.setupVersionAndPublishing
+import de.honoka.gradle.buildsrc.Versions
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    //plugins块中不会读取import语句中导入的类
+    //引入Versions对象时，必须像这样引入（https://github.com/gradle/gradle/issues/9270）
+    @Suppress("RemoveRedundantQualifierName")
+    val versions = de.honoka.gradle.buildsrc.Versions
+    //plugins
     java
     `maven-publish`
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version versions.kotlin
 }
 
 group = "de.honoka.lavender"
@@ -20,6 +26,8 @@ allprojects {
     }
 
     dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}")
+        implementation("org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}")
         //Test
         testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
         testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
@@ -31,7 +39,7 @@ allprojects {
         }
 
         withType<KotlinCompile> {
-            kotlinOptions.jvmTarget = "1.8"
+            kotlinOptions.jvmTarget = java.targetCompatibility.toString()
         }
 
         test {
---
+++ buildSrc/src/main/java/de/honoka/gradle/buildsrc/Versions.kt
@@ -0,0 +1,7 @@
+package de.honoka.gradle.buildsrc
+
+@Suppress("ConstPropertyName")
+object Versions {
+
+    const val kotlin = "1.6.21"
+}
